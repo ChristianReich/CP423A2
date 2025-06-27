ccfc2dfab0c3c60a054f744a2f61bf0128370ca3
Christian Reichenbach <christianreich2001@gmail.com>
2025-06-27 12:05:25 -0400

    create all TF weighting into df's

--- mainQ2.py
+++ mainQ2.py
@@ -1,6 +1,7 @@
 from preprocessing import preprocess_text, process_docs
 from pathlib import Path
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 
@@ -18,13 +19,25 @@ def main():
     text_data = [' '.join(tokens) for tokens in docs.values()] # CountVectorizer expects a list of strings
 
     # Initialize vectorizer with binary mode
-    vectorizer = CountVectorizer(binary=True)
+    vectorizer_binary = CountVectorizer(binary=True)
 
     # Fit and transform
-    X = vectorizer.fit_transform(text_data)
+    X_binary = vectorizer_binary.fit_transform(text_data)
 
     # Convert to DataFrame
-    df_binary = pd.DataFrame(X.toarray(), index=doc_names, columns=vectorizer.get_feature_names_out())
+    binary_df = pd.DataFrame(X_binary.toarray(), index=doc_names, columns=vectorizer_binary.get_feature_names_out())
+
+    vectorizer_count = CountVectorizer()
+        # Fit and transform
+    X_count = vectorizer_count.fit_transform(text_data)
+
+    # Convert to DataFrame
+    count_df = pd.DataFrame(X_count.toarray(), index=doc_names, columns=vectorizer_count.get_feature_names_out())
+    
+    norm_df = count_df.div(count_df.sum(axis=1), axis=0)
+    log_df = np.log1p(count_df)
+    dubnorm_df = 0.5 + 0.5 * (count_df.div(count_df.max(axis=1), axis=0))
+    print(dubnorm_df[dubnorm_df["00"] > 0.5].head())
     return
 
 if __name__ == "__main__":
